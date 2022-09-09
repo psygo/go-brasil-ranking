e3fec76733297e736f8b393a55ffc15e6c7072ca
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-09 13:28:18 -0300

    first dummy cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,25 @@
-// import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import express from "express";
+
+admin.initializeApp();
+
+export const db = admin.firestore();
+
+const goBrasilRankingApp = express();
+
+goBrasilRankingApp.get("/", (_, res) => {
+  try {
+    res.status(200).send({
+      status: "success",
+      message: "Hello there",
+    });
+  } catch (e) {
+    const error = e as Error;
+
+    res.status(500).json(error.message);
+  }
+});
+
+export const goBrasilRanking = functions.https.onRequest(goBrasilRankingApp);
