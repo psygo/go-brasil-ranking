659bfb6968d04727bbd4721d06e32f9da0a33fdd
Philippe Fanaro <philippefanaro@gmail.com>
2022-09-16 13:29:42 -0300

    more improvements to the about page

--- go_brasil_ranking/src/ui/views/about-view.ts
+++ go_brasil_ranking/src/ui/views/about-view.ts
@@ -5,19 +5,61 @@ export default class AboutView extends HTMLElement {
     document.title = "RBGo | Sobre";
 
     this.setContent();
+    this.numberFaqQuestions();
+  }
+
+  private static readonly moderatorEmail: string = "moderador@rbgo.com.br";
+  private static readonly repoAddress: string =
+    "https://github.com/psygo/ranking-brasileiro-de-go";
+
+  private get moderatorLinkString(): string {
+    return `<a href="mailto:${AboutView.moderatorEmail}">moderador</a>`;
   }
 
   private setContent = (): void => {
     this.innerHTML = `
       <h1>Sobre o Ranking Brasileiro de Go (RBGo)</h1>
+      
+      <h2>Como faço para ser incluído no RBGo?</h2>
+      
+        <p>
+           Mande um email para o ${this.moderatorLinkString}
+           com os seus dados:
+        </p>
 
-      <h2>1. Como adicionar uma partida ao Ranking Brasileiro de Go?</h2>
+        <ul>
+          <li>Nome completo</li>
+          <li>Nível em um dos servidores conhecidos (KGS, OGS, Fox, Tygem, etc.)</li>
+          <li>3 partidas recentes</li>
+        </ul>
+
+      <h2>Como adicionar uma partida ao Ranking Brasileiro de Go?</h2>
       
-      <p>Mande uma mensagem com a sua partida para o moderador: </p>
+        <p>
+          Após ter feito seu cadastro, mande uma mensagem com a sua partida
+          para o ${this.moderatorLinkString} com os dados da partida e,
+          preferencialmente, o arquivo SGF.
+        </p>
       
-      <h2>2. Quero ajudar a melhorar o RBGo. Por onde posso fazê-lo?
+      <h2>Quero ajudar a melhorar o RBGo. Por onde posso fazê-lo?</h2>
       
-      <p></p>
+        <p>
+          O RBGo é um projeto em código aberto, ou seja, a infraestrutura é
+          inspecionável por qualquer um. Se você quiser e puder acrescentar ou
+          melhorar alguma funcionalidade é só 
+            <a href="${AboutView.repoAddress}">
+              adicionar um <i>pull request</i> no Github
+            </a>.
+        </p>
     `;
+  };
+
+  private numberFaqQuestions = (): void => {
+    const questions = this.querySelectorAll("h2");
+
+    for (let i = 0; i < questions.length; i++) {
+      const question = questions[i];
+      question.innerHTML = `${i + 1}. ${question.innerHTML}`;
+    }
   };
 }
